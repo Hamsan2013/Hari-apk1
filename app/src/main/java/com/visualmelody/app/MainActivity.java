5d37e04db13e9310f63d71fa886d9db2bc33f58b
Hamsan2013 <hamsandivya@gmail.com>
2026-03-08 10:56:43 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/visualmelody/app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.hari.en.ad.app;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.loadUrl("https://visual-melody.lovable.app/");
+    }
+}
